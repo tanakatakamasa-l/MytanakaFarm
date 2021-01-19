3f62f47fc3fae4ec86ba702227b65fbac6e82fd2
tanakatakamasa-l <74941080+tanakatakamasa-l@users.noreply.github.com>
2021-01-19 14:46:57 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -49,5 +49,26 @@ $(function(){
     });
   });
 
+  function demo01() {
+    $(this).next().slideToggle(300);
+    }
+    
+  $(".toggle").click(demo01);
 
+  function demo02() {
+    $(this).toggleClass('active').next().slideToggle(300);
+    }
+    
+  $(".switch .toggle").click(demo02);
+
+
+  
 });
+    jQuery(function ($) {
+      $(".accordion-content").css("display", "none");
+      
+      $(".js-accordion-title").click(function () {
+        $(this).toggleClass("open");
+        $(this).next().slideToggle(300);
+      });
+    });
